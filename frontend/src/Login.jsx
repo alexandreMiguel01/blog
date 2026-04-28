40fe3eca0411a741f5b1bdf0d00ea5654ea632ba
Alexandre Miguel <alexandresantos9214@gmail.com>
2026-04-28 16:25:08 -0300

    Corrige API URL frontend

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./Login.css";
+const API_URL = "https://blog-5mz2.onrender.com";
 
 function Login({ setToken, setUserId, goToRegister }) { // 👈 AQUI
   const [email, setEmail] = useState("");
